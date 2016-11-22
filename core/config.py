f46c33939333170eaa818e472f4c38b60ebbf945
PengfTH <pengfth@gmail.com>
2016-11-23 01:51:50 +0800

    remove aws key secret for security issue

--- core/config.py
+++ core/config.py
@@ -1,13 +1,8 @@
-es_host = 'search-twitterma-demo-3jefbxfb73i3cphbsiy5prjxpu.us-west-2.es.amazonaws.com'
+es_host = <elasticsearch endpoint>
 es_port = 443
 es_bulk_chunk_size = 1000  #number of documents to index in a single bulk operation
 
-es_access_key = 'AKIAINFSC67FK56IAHTQ'
-es_access_secret = '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'
-es_region = 'us-west-2'
+es_access_key = <aws access key>
+es_access_secret = <aws access secret>
+es_region = <elasticsearch region>
 es_name = 'es'
-
-twitter_consumer_key = 'CnuMJRz6wr1ppxs0XWUMXPJtC'
-twitter_consumer_secret = 'ys6MEaidZiA7g57jRP7Sp1s45dkf5bzRDmd9AQPpiECUFqmeem'
-twitter_access_token = '783379170810290176-maE3Eqiv3K2Nj9Mm3ehEfNvXdnschSZ'
-twitter_access_token_secret = 'ahIrQ0UoRPf90F8527N0ghveBd213S2qJHfKTAgTwFbER'
