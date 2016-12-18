fdfddda1f7c7649b74ae63bbe3b2d7b42e7a51d6
Ye Eun Jeong <zoey.jeong@gmail.com>
2016-12-18 11:13:36 -0500

    adding script

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# assumes that docker is already installed
+
+FILE=output.txt
+touch $FILE
+
+
+#if [ $# != 2 ]
+docker pull $1
+cp 
+docker run -i -t $1 /bin/bash -c "dpkg --get-selections | grep -v deinstall | cut -f1" > $FILE
+
+exit 0
