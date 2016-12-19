484abec31aaa1653fc2b7a0043da197a7700c637
Ubuntu <ubuntu@ip-172-31-53-6.ec2.internal>
2016-12-19 03:29:08 +0000

    Script

---
+++ findPackages.sh
@@ -0,0 +1,20 @@
+IMAGE=$1;
+
+docker pull $IMAGE
+AAA="$(docker run -it $IMAGE dpkg --get-selections | grep -v deinstall )"
+if [[ $AAA != *"not found"* ]]
+then
+  echo "${AAA}" | awk {'print $1'} > file.txt;
+  exit;
+fi
+
+BBB="$(docker run -it $IMAGE rpm -qa)"
+if [[ $BBB != *"not found"* ]]
+then
+  echo "${BBB}" > file.txt;
+  exit;
+else
+ echo "No supported package manager found on image" > file.txt;
+fi
+
+
